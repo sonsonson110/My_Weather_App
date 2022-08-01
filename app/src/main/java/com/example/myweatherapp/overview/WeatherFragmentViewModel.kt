da4801ecec7ac62381d1c3fd4c5bf6e86388ef86
Pson <pson141002@gmail.com>
2022-08-01 23:50:32 +0700

    Weather Info test & sync message

--- app/src/main/java/com/example/myweatherapp/overview/WeatherFragmentViewModel.kt
+++ app/src/main/java/com/example/myweatherapp/overview/WeatherFragmentViewModel.kt
@@ -5,21 +5,35 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.myweatherapp.api.RetrofitInstance
+import com.example.myweatherapp.model.ApiData
 import kotlinx.coroutines.launch
+//sync status
+enum class CurrentStatus { LOADING, DONE, ERROR }
 
 class WeatherFragmentViewModel : ViewModel() {
 
     //LiveData response
-    private val _myResponse = MutableLiveData<String>("Hi")
-    val myResponse: LiveData<String> = _myResponse
+    private val _myResponse = MutableLiveData<ApiData>()
+    val myResponse: LiveData<ApiData> = _myResponse
+
+    //sync progress
+    private val _status = MutableLiveData<CurrentStatus>()
+    val status: LiveData<CurrentStatus> = _status
+
+    //location properties
+    private val _city = MutableLiveData<String>()
+    val city: LiveData<String> = _city
 
     fun getData(customLocation: String) {
         viewModelScope.launch {
+            _status.value = CurrentStatus.LOADING
             try {
+                //full response data
                 val response = RetrofitInstance.retrofitService.getLocationDetail(customLocation)
-                _myResponse.value = response.location?.name!!
+                _city.value = response.location!!.name!!
+                _status.value = CurrentStatus.DONE
             } catch (e: Exception) {
-                _myResponse.value = "Fetching failure"
+                _status.value = CurrentStatus.ERROR
             }
         }
     }
